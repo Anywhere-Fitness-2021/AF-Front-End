d957ea10b049f44fddeda81d3b6d0ed79024b323
ricardo-pena <ricardo.penagarcia06@gmail.com>
2021-04-27 13:32:32 -0400

    Home Landing Page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,14 +2,29 @@
 import React from "react";
 //STYLING IMPORTS
 import "../src/index.css";
-import Logo from "./AnywhereFitnessLogo.PNG"
+import Logo from "./AnywhereFitnessLogo.PNG";
+import HomePage from "./HomePage";
+import { BrowserRouter as Router , Link, Switch, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-   <div>
-     <div className="logoAndHeading"></div>
+   <div className ="App">
+     <nav>
      <img className="logo" src={Logo} alt="gym barbell"/>
-     <h1 className="mainHeading">Anywhere Fitness</h1>
+      <h1 className="mainHeading">Anywhere Fitness</h1>
+      <div className ="navLinks">
+          <Link to='/create-account'>Sign Up</Link>
+          <Link to ='/login'>Log In</Link> 
+      </div>
+      </nav>
+
+
+     {/* <div className="logoAndHeading"></div>
+     <img className="logo" src={Logo} alt="gym barbell"/>
+     <h1 className="mainHeading">Anywhere Fitness</h1> */}
+    <HomePage/>
+
    </div>
   );
 }
