3334bfde72576e49ed74e41e4bc35d6c02eb384a
Sergio Alvarez <salvare736@gmail.com>
2021-04-28 20:18:46 -0400

    I messed up the main branch trying to fix it after the latest feature merge, so I'm creating this master branch to serve as the new default branch

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -2,17 +2,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from "react-router-dom";
-//STYLING IMPORTS
-import './index.css';
-//COMPONENT IMPORTS
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 
+//STYLING IMPORTS
+import './index.css';
+
+//COMPONENT IMPORTS
+import App from './App';
 import { reducer } from './store';
 
 const store = createStore(reducer, applyMiddleware(thunk, logger));
@@ -25,8 +24,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
